da62a927e53b4f8592814380d08f62f85db65b4a
AnHii <thuongkhungcuukyquyet@gmail.com>
2025-07-11 16:28:54 +0700

    goi div o main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 print('hello world from python')
 
-from math_utils import add, subtract, multi
+from math_utils import add, subtract, multi, div
 
 
 print(add(10, 5))
 print(subtract(10, 5))
 print(multi(10, 5))
+print(div(10, 5))
