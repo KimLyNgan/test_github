af9206d71e2cfe42e8b243e611d5bfe67a068947
AnHii <thuongkhungcuukyquyet@gmail.com>
2025-07-11 16:04:46 +0700

    them ham nhan, sua lai main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ print('hello world from python')
 print('sua tay 1 doan code de tao conflic')
 print("Teammate changed this directly on main branch!")
 print("Do not lose this when merging!")
+
+from math_utils import add, subtract, multi
+print(add(10,5))
+print(subtract(10, 5))
+print(multi(10, 5))
